a2933f2e17f6378147386ded9781d48a5b5e534d
Rick Hwang <rickhwang@91app.com>
2018-12-13 12:40:53 +0800

    update on 20181213-1240, comment:

--- build_docs.sh
+++ build_docs.sh
@@ -5,3 +5,6 @@ mkdocs build --clean
 mv site books
 
 #cp -R ./docs/pdf/ site/
+cd books
+find . -type f -name '*.html' -exec sed -i '' s/pdf/sre\\/pdf/ {} +
+find . -type f -name '*.html' -exec sed -i '' s/act/sre\\/act/ {} +
