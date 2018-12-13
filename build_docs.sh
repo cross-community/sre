7144ff6eb50973efdb24f2823962f9355484ed99
Rick Hwang <rickhwang@91app.com>
2018-12-13 12:43:30 +0800

    update on 20181213-1243, comment:

--- build_docs.sh
+++ build_docs.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 rm -rf books
-mkdocs build --clean
-mv site books
+rm -rf act
+rm -rf pdf
 
-#cp -R ./docs/pdf/ site/
+## Build
+mkdocs build --clean
+
+## structure folder.
+mv site books
+mv books/pdf .
+mv books/act .
+
+## Replace
 cd books
+# for macOS only. see https://stackoverflow.com/questions/9704020/recursive-search-and-replace-in-text-files-on-mac-and-linux
 find . -type f -name '*.html' -exec sed -i '' s/pdf/sre\\/pdf/ {} +
 find . -type f -name '*.html' -exec sed -i '' s/act/sre\\/act/ {} +
